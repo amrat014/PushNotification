1c70e23048ba189e67c3e36d519f4cb2e63542c9
Amrat Mali <amrat014@gmail.com>
2021-03-05 13:05:29 +0530

    Initial commit

---
+++ app/src/main/java/com/oneroofit/pushnotification/GettingDeviceTokenService.java
@@ -0,0 +1,20 @@
+package com.oneroofit.pushnotification;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdReceiver;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class GettingDeviceTokenService extends FirebaseInstanceIdService {
+
+
+    @Override
+    public void onTokenRefresh() {
+      String DeviceToken = FirebaseInstanceId.getInstance().getToken();
+      Log.d("DEVICE TOKEN",DeviceToken);
+    }
+}
